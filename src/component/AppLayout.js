3577c0b2a006a9425741a560bb219b873f667d17
Aditya Singh <143535978+ADITYAL1@users.noreply.github.com>
2024-05-21 05:11:18 +0000

    add css and pagination in search

--- src/component/AppLayout.js
+++ src/component/AppLayout.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import Header from './Header'
-import Popular from './Popular'
-import usePopularmovies from '../hooks/usePopularmovies'
-import useUpcoming from '../hooks/useUpcoming'
-import useTopRated from '../hooks/useTopRated'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import Header from "./Header";
+import Popular from "./Popular";
+import usePopularmovies from "../hooks/usePopularmovies";
+import useUpcoming from "../hooks/useUpcoming";
+import useTopRated from "../hooks/useTopRated";
+import { Outlet } from "react-router-dom";
 const AppLayout = () => {
   usePopularmovies();
-useUpcoming();
-useTopRated();
+  useUpcoming();
+  useTopRated();
   return (
-    <div className=' bg-gray-500'>
-      <Header/>
-      <Outlet/>
+    <div className=" bg-gray-500  ">
+      <Header />
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default AppLayout
+export default AppLayout;
--- src/component/Search.js
+++ src/component/Search.js
@@ -1,32 +1,41 @@
-
-import React from 'react'
-import MovieCard from './MovieCard'
-import { useDispatch, useSelector } from 'react-redux';
-import Pagination from './Pagination';
-import { addSeCurPage, addSePrevPage } from '../utils/moviesSlice';
-import useSearch from '../hooks/useSearch';
+import React from "react";
+import MovieCard from "./MovieCard";
+import { useDispatch, useSelector } from "react-redux";
+import Pagination from "./Pagination";
+import { addSeCurPage, addSePrevPage } from "../utils/moviesSlice";
+import useSearch from "../hooks/useSearch";
 
 const Search = () => {
   useSearch();
- const dispatch=useDispatch();
-  const movies= useSelector((store)=>store.movies.search);
-  const page= useSelector((store)=>store.movies.searchpage);
-  const totalpages= useSelector((store)=>store.movies.totalsearchpage);
-  const onnextpage=()=>{
+  const dispatch = useDispatch();
+  const movies = useSelector((store) => store.movies.search);
+  const page = useSelector((store) => store.movies.searchpage);
+  const totalpages = useSelector((store) => store.movies.totalsearchpage);
+  const onnextpage = () => {
     dispatch(addSeCurPage());
-    }
-    const onprevpage=()=>{
-     dispatch(addSePrevPage());
-    }
+  };
+  const onprevpage = () => {
+    dispatch(addSePrevPage());
+  };
   return (
-    <div className=' h-screen md:h-auto md:mt-2  md:ml-28 md:mr-6'>
-     
-     <div className=' flex  flex-wrap'>
-         { movies?.map((movie) =>(
-          <MovieCard key={movie.id}  id={movie.id} posterPath={movie.poster_path} title={movie.title} rating={movie.vote_average}/>))}
-         </div>
-         <Pagination currentPage={page} totalPages={totalpages} nextDispatch={onnextpage} prevDispatch={onprevpage}/>
-
+    <div className="  md:h-auto md:mt-2  md:ml-28 md:mr-6">
+      <div className=" flex  flex-wrap">
+        {movies?.map((movie) => (
+          <MovieCard
+            key={movie.id}
+            id={movie.id}
+            posterPath={movie.poster_path}
+            title={movie.title}
+            rating={movie.vote_average}
+          />
+        ))}
+      </div>
+      <Pagination
+        currentPage={page}
+        totalPages={totalpages}
+        nextDispatch={onnextpage}
+        prevDispatch={onprevpage}
+      />
     </div>
   );
 };
